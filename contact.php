077ee9c2e12fcc930c0459c39e2db1205eb73f36
COMPUTER 01 <siriporn02>
2022-10-09 11:32:32 +0700

    Updated3

--- contact.php
+++ contact.php
@@ -15,15 +15,11 @@
     <?php
       include "navbar.php";
     ?>
-
-    <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.1650620557198!2d100.5546496147119!3d13.76891760060652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29ebdd673c69d%3A0xee38b1d37cda3c91!2z4LmC4Lij4LiH4LmA4Lij4Li14Lii4LiZ4Lid4Li24LiB4Lit4Liy4LiK4Li14Lie4LiB4Lij4Li44LiH4LmA4LiX4Lie4Lih4Lir4Liy4LiZ4LiE4LijKOC4lOC4tOC4meC5geC4lOC4hzEp!5e0!3m2!1sen!2sth!4v1662553535587!5m2!1sen!2sth"
-        width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade">
+<br><br><center>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.9486074151173!2d100.5297080148548!3d13.721561301690341!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29f32f343d10b%3A0x7ea9cce6816151e!2z4Liq4Liy4LiX4Lij4LiL4Li04LiV4Li14LmJIOC4l-C4suC4p-C5gOC4p-C4reC4o-C5jCDguYHguILguKfguIcg4LiX4Li44LmI4LiH4Lih4Lir4Liy4LmA4Lih4LiGIOC5gOC4guC4lSDguKrguLLguJfguKMg4LiB4Lij4Li44LiH4LmA4LiX4Lie4Lih4Lir4Liy4LiZ4LiE4LijIDEwMTIw!5e0!3m2!1sth!2sth!4v1665289152153!5m2!1sth!2sth" width="80%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </center>
+  <div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="80%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=sathorn&amp;t=h&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://mcpenation.com/">https://mcpenation.com</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>
       
-  </iframe>
-  <div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=sathorn&amp;t=h&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://mcpenation.com/">https://mcpenation.com</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>
-        
 </body>
 
 </html>
